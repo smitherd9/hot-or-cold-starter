8e7bd5633842d2b1f9a840ff7c37e9ccaf2fe79a
Daniel Smither <smitherd9@gmail.com>
2016-07-28 10:24:35 -0400

    First commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function(){
-	
+
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -11,6 +12,71 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+
+var secretNum;
+
+  	// var secretNum = Math.floor((Math.random() * 100) + 1);
+
+  	function newGame(){
+		randomNum();
+		$('#count') === 0;
+	}
+
+	function randomNum() {
+		secretNum = Math.floor((Math.random() * 100) + 1);
+		console.log('You generated a random number.')
+		console.log(secretNum);
+	}
+
+	$('.new').click(function() {
+		newGame();
+		console.log('You clicked New Game.')
+	});
+
+  		
+	$('form').submit(function(event){
+		event.preventDefault();
+		var input = parseInt($('form').val(), 10);
+		$('form').val("");
+		
+		console.log('You submitted ' + input);
+
+
+		if (input != secretNum) {
+			$('.guessBox').append(input);
+			var count = $('#count');
+			count++;
+			alert('Try Again!');
+		}
+
+		else {
+			$('#feedback').text('Congrats!  You won!');
+		
+		}
+
+	});
+
+
+	// $('#guessButton').click(function(event){
+	// 	event.preventDefault();
+	// 	var input = parseInt($('#guessButton').val(), 10);
+		
+
+	// 	if (input != secretNum) {
+	// 		alert('Try Again!');
+
+	// 	}
+
+	// });
+
+
+	// $('#count')function userGuess() {
+	// 	if (input != secretArray) {
+	// 		append()
+	// 	}
+	// }
+
+	
 
 });
 
