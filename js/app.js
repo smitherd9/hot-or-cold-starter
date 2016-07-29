ab984526f2c638c7ccf1c40aa09fffe663a3f0a0
Daniel Smither <smitherd9@gmail.com>
2016-07-29 11:58:27 -0400

    Updated hot or cold code and added case for user repeating a number

--- js/app.js
+++ js/app.js
@@ -13,11 +13,17 @@ $(document).ready(function() {
     });
 
     var secretNum;
+    // randomNum() must be called before var plusFive and minusFive
+    // are defined so that secretNum is == to a number before it is placed in plusFive, minusFive.  
+    // If not, secretNum within plusFive/minusFive = NaN.  
+    // In the hot or cold section, input > NaN always evaluates to false.
+    randomNum();
     var count = 0;
-    var plusFive = secretNum + 5;
+    var plusFive = secretNum + 5; // undefined + 5
     var minusFive = secretNum - 5;
+    var guessArray = [];
 
-    
+
 
     function newGame() {
         randomNum();
@@ -30,7 +36,7 @@ $(document).ready(function() {
     function randomNum() {
         secretNum = Math.floor((Math.random() * 100) + 1);
         console.log('You generated a random number.')
-        console.log(secretNum);
+            //console.log(secretNum);
     }
 
     $('.new').click(function() {
@@ -42,117 +48,60 @@ $(document).ready(function() {
     $('form').submit(function(event) {
         event.preventDefault();
         var input = parseInt($('#userGuess').val(), 10); // Submit the form, parse the actual input number
+        // check to see whether input exists in some guesses array.
+        // if it is not there, then push it to the global array
+
         $('#userGuess').val("");
 
+        // Checks if a user guess exists in array,
+        // if indexOf = -1 it does not exist and therefore pushes input to array.
+        if (guessArray.indexOf(input) == -1) {  
+            guessArray.push(input); 
+
+            if (input == secretNum) {
+                win();
+            } else if (((input > secretNum) && (input <= plusFive)) || ((input < secretNum) && (input >= minusFive))) {
+                hot();
+                continueGame();
+            } else {
+                cold();
+                continueGame();
+            };
+        } else {
+            $('h2#feedback').text("Don't repeat yourself!");
+        }
+
         console.log('You submitted ' + input);
-        
 
-        if ((input > secretNum) && (input < plusFive)) {
-            hot();
-            continueGame();
-        }
 
-        else if ((input < secretNum) && (input > minusFive)) {
-        	hot();
-        	continueGame();
-        }
 
-        else if ((input > plusFive) || (input < minusFive)) {
-        	cold();
-        	continueGame();
-        }
 
-        else if (input == secretNum) {
-        	win();
+
+
+        function continueGame() {
+            $('ul#guessList').append('<li>' + input + '</li>');
+            count++; // Increments the count number 
+            $('#count').text(count); // Set this to print on screen
         };
-
-
-    
-
-    function continueGame() {
-        $('ul#guessList').append('<li>' + input + '</li>');
-        count++; 				 // Increments the count number 
-        $('#count').text(count); // Set this to print on screen
-    };
 
     });
 
 
     function hot() {
-    	$('h2#feedback').text("You're hot!");
-    	
+        $('h2#feedback').text("You're hot!");
+
     };
 
     function cold() {
         $('h2#feedback').text("You're cold!")
-        
+
     };
 
     function win() {
         $('h2#feedback').text('Congrats!  You won! Click "New Game" to play again.');
     };
 
-    //         plusFive = input ? (hot());
-    //     minusFive = input ? (hot())
-    //     plusFive ? != input : cold();
-    //     minusFive ? != input : cold();
-
-    //     input ? = secretNum : win();
-
-    // });
-
-
     
-
-    // input == secretNum + 5 ? (
-    // 	$('h2#feedback').text("You're hot!"), 
-    // 		$('ul#guessList').append('<li>' + input + '</li>'),
-    // 		count++,					// Increments the count number 
-    // 		$('#count').text(count)	// Set this to print on screen
-    // 	) 
-    // });
-
-    // 	if (input = secretNum) {
-    // 		$('h2#feedback').text('Congrats!  You won! Click "New Game" to play again.');
-    // 	}
-
-    // 	else if (input > secretNum && < (secretNum - 5) ||  (secretNum  + 5)) {
-    // 		$('h2#feedback').text("You're hot!"); 
-    // 		$('ul#guessList').append('<li>' + input + '</li>');
-    // 		count++;					// Increments the count number 
-    // 		$('#count').text(count);	// Set this to print on screen		
-
-    // 	}
-
-    // 	else 	{
-    // 		$('h2#feedback').text("You're cold!") 
-    // 		$('ul#guessList').append('<li>' + input + '</li>');
-    // 		count++;					// Increments the count number 
-    // 		$('#count').text(count);	// Set this to print on screen		
-    // 	}
-
-    // });
-
-
-    // $('#guessButton').click(function(event){
-    // 	event.preventDefault();
-    // 	var input = parseInt($('#guessButton').val(), 10);
-
-
-    // 	if (input != secretNum) {
-    // 		alert('Try Again!');
-
-    // 	}
-
-    // });
-
-
-    // $('#count')function userGuess() {
-    // 	if (input !== secretArray) {
-    // 		append()
-    // 	}
-    // }
-
 
 
 });
